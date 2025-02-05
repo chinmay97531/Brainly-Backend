b3a8db5628650c4031ab66c7d1504815c8e34ff7
Chinmay <141347159+chinmay97531@users.noreply.github.com>
2025-02-06 00:18:51 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const JWT_SECRET = "Xebia2022@2026@2044";
-export const MONGODBURL = "mongodb+srv://ChinmayMittal:mBVwCN9LYdL6pIgz@cluster0.8mqmi.mongodb.net/brainly";
+export const JWT_SECRET = "Your JWT_SECRET";
+export const MONGODBURL = "Your MongoDB URL";
