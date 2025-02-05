8b27ca2de9ad022d0a7620aaaeda593149e3b57e
Chinmay <141347159+chinmay97531@users.noreply.github.com>
2025-02-06 00:20:08 +0530

    Delete dist directory

--- dist/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGODBURL = exports.JWT_SECRET = void 0;
-exports.JWT_SECRET = "Xebia2022@2026@2044";
-exports.MONGODBURL = "mongodb+srv://ChinmayMittal:mBVwCN9LYdL6pIgz@cluster0.8mqmi.mongodb.net/brainly";
